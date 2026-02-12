e337244e0acd692bac859976da833abc37db4114
Joe-Yang1 <348993072@tdsb.ca>
2026-02-12 16:21:35 +0000

    Progress

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,10 @@
+/**
+* File Name: Lesson 1.3 - Strings
+* Author: Joe Yang
+* Date Created: Feb 8, 2023
+* Date Last Modified: Feb 12, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,19 +16,34 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+	System.out.println(" * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n * * * * * *\n* * * * * *\n");
+	System.out.println("* * * * *\n*       *\n*       *\n*       *\n* * * * *\n");
+	System.out.println("* * * * * *\n *       *\n  *     *\n   *   *\n    * *\n     *\n");   
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+	System.out.println("		          '__'\n		          (oo)\n		  /========//\n		 / || XX ||\n		*  ||----||\n		   VV    VV\n		   ''    ''");
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+	String game = "Fortnite ";
+	String text = "Is awful";
+	System.out.print(game);
+	System.out.println(text);
+
+	
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+	String word = "Don't ";
+	String word2 = "@ me";
+	System.out.print(word + word2);
+	
 	}
+
 
 
 }
